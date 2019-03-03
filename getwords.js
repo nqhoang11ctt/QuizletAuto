9f922729f7c59508922176244227753819aa210d
Hoang <nqhoang11ct@gmail.com>
2019-03-03 16:30:48 +0700

    Load CSV File and fill in Quizlet

--- getwords.js
+++ getwords.js
@@ -1,22 +1,28 @@
 const csv = require('csv')
 const fs = require('fs')
 
-const parseWordCSV = async (path) => {
-    let words = await new Promise((resolve, reject)=>{
-        var data = []
+getwords = (path) => {
+    let words = new Promise((resolve, reject)=>{
+        var data = ""
         fs.createReadStream(path)
         .pipe(csv.parse({
             delimiter: ','
         }))
         .on("readable", function() {
             while (row = this.read()){
-                data.push({word: row[0], def: row[1]});
+                data += `${row[0]}\t${row[1]}\n`;
             }
         })
         .on("finish", function(){
             resolve(data);
         })
+        .on("error", function(){
+            reject(new Error("Error reading CSV file!"));
+        })
     });
     return words;    
 }
 
+getwords("words.csv").then(console.log);
+
+module.exports = getwords;
--- quizlet.js
+++ quizlet.js
@@ -1,84 +1,84 @@
 const puppeteer = require('puppeteer');
+const getwords = require(__dirname + "/getwords.js");
 const USERNAME = "Hoang_Nguyen402";
 const PASSWORD = "3FDzSvzhJAcd5at";
 var WORD_LANGUAGE = "Vietnamese";
 var DEF_LANGUAGE = "English";
 var wordsFile = __dirname + '/words.csv';
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: [ '--use-fake-ui-for-media-stream' ]
-    });
-    const page = await browser.newPage();
-    await page.goto('https://quizlet.com/login');
-    //   await page.screenshot({path: 'example.png'});
-
-    // LOGIN
-    const USERNAME_SELECTOR = "input[name='username']";
-    const PASSWORD_SELECTOR = "input[name='password']";
-    const SUBMIT_SELECTOR = "button[type='submit']";
-
-    await page.click(USERNAME_SELECTOR);
-    await page.keyboard.type(USERNAME);
-
-    await page.click(PASSWORD_SELECTOR);
-    await page.keyboard.type(PASSWORD);
-
-    await page.click(SUBMIT_SELECTOR);
-    await page.waitForNavigation();
-
-    const createUrl = "https://quizlet.com/create-set";
-    await page.goto(createUrl);
-
-    // Set Study Set Title
-
-    const TITLE_SELECTOR = "div.AutoExpandTextarea-wrapper > textarea";
-    await page.click(TITLE_SELECTOR);
-    await page.keyboard.type("My Study Set 31");
-
-    // Import Words and Definitions
-
-    const BULKADD_SELECTOR = "div.CreateSetHeader > div:nth-child(3) > div > button";
-    await page.click(BULKADD_SELECTOR);
-    await page.keyboard.type("WORD 3\tDEF3\nWORD4\tDEF4");
-    await page.waitFor(2*1000);
-
-    const IMPORT_SELECTOR = "div.ImportTerms-importButtonWrap > button";
-    await page.click(IMPORT_SELECTOR);
-
-    await page.waitFor(4*1000); 
-
-    // Set Word & Definition Language
-    // Activate Lang selector
-
-    await page.click(".TermRows textarea");
-    // Word Lang
-    await page.click("div.TermContent-side.TermContent-side--word > div > div > label > span > div > button");
-    // await page.click("div.LanguageSelect-input > label > div > input");
-    await page.keyboard.type(WORD_LANGUAGE);
-    await page.keyboard.press("Enter");
+getwords("words.csv")
+.then(
+    async (words) => {
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: [ '--use-fake-ui-for-media-stream' ]
+        });
+        const page = await browser.newPage();
+        await page.goto('https://quizlet.com/login');
+        //   await page.screenshot({path: 'example.png'});
     
-    await page.waitFor(1000);
-
-    // // Def Lang
-    const DEFLANG_SELECTOR = "div.TermContent-side.TermContent-side--definition > div > div > label > span > div > button";
-    await page.click(DEFLANG_SELECTOR);
-    // await page.click("div.LanguageSelect-input > label > div > input");
-    await page.keyboard.type(DEF_LANGUAGE);
-    await page.keyboard.press("Enter");
+        // LOGIN
+        const USERNAME_SELECTOR = "input[name='username']";
+        const PASSWORD_SELECTOR = "input[name='password']";
+        const SUBMIT_SELECTOR = "button[type='submit']";
     
-    await page.waitFor(1000);
+        await page.click(USERNAME_SELECTOR);
+        await page.keyboard.type(USERNAME);
     
-    // Record
+        await page.click(PASSWORD_SELECTOR);
+        await page.keyboard.type(PASSWORD);
+    
+        await page.click(SUBMIT_SELECTOR);
+        await page.waitForNavigation();
+    
+        const createUrl = "https://quizlet.com/create-set";
+        await page.goto(createUrl);
+    
+        // Set Study Set Title
+    
+        const TITLE_SELECTOR = "div.AutoExpandTextarea-wrapper > textarea";
+        await page.click(TITLE_SELECTOR);
+        await page.keyboard.type("My Study Set 31");
+    
+        // Import Words and Definitions
+    
+        const BULKADD_SELECTOR = "div.CreateSetHeader > div:nth-child(3) > div > button";
+        await page.click(BULKADD_SELECTOR);
+        await page.keyboard.type(words);
+        await page.waitFor(2*1000);
+    
+        const IMPORT_SELECTOR = "div.ImportTerms-importButtonWrap > button";
+        await page.click(IMPORT_SELECTOR);
+    
+        await page.waitFor(2*1000); 
+    
+        // Set Word & Definition Language
+        // Activate Lang selector
+    
+        await page.click(".TermRows textarea");
+        // Word Lang
+        await page.click("div.TermContent-side.TermContent-side--word > div > div > label > span > div > button");
+        // await page.click("div.LanguageSelect-input > label > div > input");
+        await page.keyboard.type(WORD_LANGUAGE);
+        await page.keyboard.press("Enter");
+        
+        await page.waitFor(1000);
+    
+        // // Def Lang
+        const DEFLANG_SELECTOR = "div.TermContent-side.TermContent-side--definition > div > div > label > span > div > button";
+        await page.click(DEFLANG_SELECTOR);
+        // await page.click("div.LanguageSelect-input > label > div > input");
+        await page.keyboard.type(DEF_LANGUAGE);
+        await page.keyboard.press("Enter");
+        
+        await page.waitFor(1000);
+        
+        // Record
+    
+        // Finally
+        const CREATE_SELECTOR  = "div.CreateSetHeader-infoButtonWrap > button";
+        // await page.click(CREATE_SELECTOR);
+        // await browser.close();
+    }
+);
 
-    // Finally
-    const CREATE_SELECTOR  = "div.CreateSetHeader-infoButtonWrap > button";
-    // await page.click(CREATE_SELECTOR);
-    // await browser.close();
-})();
-
-// Click Record Button
-// termID = 1;
-//  
-// document.querySelectorAll(`div.TermRows-termRowWrap[data-term-luid='term-${termId}'] button`)[1].click();
